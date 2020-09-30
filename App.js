a43d1de21bc626558639d1a5f4f6166fdbee24e9
fullStackOasis <fullstackdev@fullstackoasis.com>
2020-09-30 13:33:49 -0400

    Initial state of the app. In this state, the image is far too large, and you can only see the upper left corner of it on the screen.

---
+++ App.js
@@ -0,0 +1,89 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ *
+ * @format
+ * @flow strict-local
+ */
+<script src="http://localhost:8097"></script>
+import React from 'react';
+import {
+  SafeAreaView,
+  StyleSheet,
+  ScrollView,
+  View,
+  Text,
+  StatusBar,
+  Image
+} from 'react-native';
+
+import {
+  Colors
+} from 'react-native/Libraries/NewAppScreen';
+
+const App: () => React$Node = () => {
+  return (
+    <>
+      <StatusBar barStyle="dark-content" />
+      <SafeAreaView>
+        <ScrollView
+          contentInsetAdjustmentBehavior="automatic"
+          style={styles.scrollView}>
+          <View style={styles.body}>
+            <View style={styles.sectionContainer}>
+              <Text style={styles.sectionTitle}>A flower</Text>
+              <Text style={styles.sectionDescription}>
+                This is a flower image displayed in a React Native app. The original app was created using `npx react-native init AwesomeProject` and then the content was removed and mostly replaced with an image asset.
+              </Text>
+            </View>
+			<View>
+			<Image source={require('./assets/14784479665_f6ff3f76e2_o.jpg')} style={styles.responsiveImage} />
+			</View>
+          </View>
+        </ScrollView>
+      </SafeAreaView>
+    </>
+  );
+};
+
+const styles = StyleSheet.create({
+  scrollView: {
+    backgroundColor: Colors.lighter,
+  },
+  engine: {
+    position: 'absolute',
+    right: 0,
+  },
+  body: {
+    backgroundColor: Colors.white,
+  },
+  sectionContainer: {
+    marginTop: 32,
+    paddingHorizontal: 24,
+  },
+  sectionTitle: {
+    fontSize: 24,
+    fontWeight: '600',
+    color: Colors.black,
+  },
+  sectionDescription: {
+    marginTop: 8,
+    fontSize: 18,
+    fontWeight: '400',
+    color: Colors.dark,
+  },
+  highlight: {
+    fontWeight: '700',
+  },
+  footer: {
+    color: Colors.dark,
+    fontSize: 12,
+    fontWeight: '600',
+    padding: 4,
+    paddingRight: 12,
+    textAlign: 'right',
+  },
+
+});
+
+export default App;
